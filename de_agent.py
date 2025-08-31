eea5452e7e699707a658bab5a1b465938366e103
Denys <denys.nunes.gonzaga@gmail.com>
2025-08-31 16:50:52 -0300

    UI and variables
    * Variable Improvements
    * Visual improvements

--- de_agent.py
+++ de_agent.py
@@ -1,17 +1,17 @@
 import os
+import re
 import boto3
 
 from rich.console import Console
 from rich.prompt import Prompt
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.progress import Progress, BarColumn, TextColumn, TimeElapsedColumn
 
 from dotenv import load_dotenv
 
 from mcp.client.sse import sse_client
-from strands.tools.mcp import MCPClient
 
 from strands import Agent
-from strands_tools import use_aws
+from strands.tools.mcp import MCPClient
 from strands.models import BedrockModel
 
 load_dotenv()
@@ -19,42 +19,58 @@ console = Console()
 error_console = Console(stderr=True)
 
 bucket_workdir = os.getenv("AWS_DATA_BUCKET")
+reasoning_enabled = True if os.getenv("AWS_DATA_BUCKET", "false") == "true" else False
 
 DE_SYSTEM_PROMPT = f"""You are a data engineer assistant.
+You are a {os.getenv("AGENT_LANGUAGE")} speaker assistant.
 
 At this moment, you only authorized to:
 
 Only upload CSV files from ./data/ folder to s3 bucket "{bucket_workdir}" on "landing" key.
 Don't verify if file exists.
 Table (using create_table) must be created after upload (using s3_upload_file) csv, but you will create it only if requested.
-Only uses glue database named "{os.getenv("AWS_GLUE_CATALOG_DATABASE")}" 
+Only uses glue database named "{os.getenv("AWS_GLUE_CATALOG_DATABASE")}"
+Explain your reasoning.
 """
 
 session = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
-    region_name=os.getenv("AWS_DEFAULT_REGION")
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 bedrock_model = BedrockModel(
-    model_id=os.getenv("AWS_BEDROCK_MODEL_ID"),
-    boto_session=session
+    model_id=os.getenv("AWS_BEDROCK_MODEL_ID"), boto_session=session
 )
 
+
+def message_callback_controller(**kwargs):
+    if "message" in kwargs and kwargs["message"].get("role") == "assistant":
+        return kwargs["message"]["content"]
+
+
 def main():
-    sse_mcp_client = MCPClient(lambda: sse_client("http://127.0.0.1:8000/sse"))
+    sse_mcp_client = MCPClient(
+        lambda: sse_client(
+            f'http://{os.getenv("MCP_SERVER_HOST")}:{os.getenv("MCP_SERVER_PORT")}/sse'
+        )
+    )
 
     with sse_mcp_client:
         mcp_tools = sse_mcp_client.list_tools_sync()
+        console.print(
+            f"BEDROCK MODEL: [green]{os.getenv('AWS_BEDROCK_MODEL_ID')}[/green]"
+        )
+
+        err_count = 0
         while True:
             try:
-                console.print("\n", style="bold blue")
-                console.print(f"BEDROCK MODEL: [green]{os.getenv('AWS_BEDROCK_MODEL_ID')}[/green]")
-    
                 Prompt.prompt_suffix = " => "
-                prompt_input = Prompt.ask("[bold yellow]Data Engineer Agent[/bold yellow]")
-                
+                prompt_input = Prompt.ask(
+                    "[bold yellow]Data Engineer Agent[/bold yellow]"
+                )
+
                 if len(prompt_input.strip()) == 0:
                     continue
 
@@ -62,11 +78,13 @@ def main():
                     model=bedrock_model,
                     system_prompt=DE_SYSTEM_PROMPT,
                     tools=[mcp_tools],
+                    callback_handler=message_callback_controller,
                 )
-                
+
                 with Progress(
-                    SpinnerColumn(),
                     TextColumn("[progress.description]{task.description}"),
+                    BarColumn(pulse_style="bar.pulse"),
+                    TimeElapsedColumn(),
                     transient=True,
                     console=console,
                 ) as progress:
@@ -76,15 +94,32 @@ def main():
                     response = de_agent(prompt_input)
                     progress.update(task, description="[green]Done!")
 
-                console.print("\n[bold green]Agent Response:[/bold green]")
-                console.print(response, style="white on black")
-                raise Exception("Teste")
+                m = re.search(r"<thinking>(.*?)</thinking>", str(response), re.DOTALL)
+                thinking = m.group(1).strip() if m else None
+                message = re.sub(
+                    r"<thinking>.*?</thinking>", "", str(response), flags=re.DOTALL
+                ).strip()
+
+                print_message = message if message.strip() != "" else thinking
+                console.print(
+                    f"[bold blue]MESSAGE:[/bold blue] {print_message}",
+                    style="white on black",
+                )
+
+                if reasoning_enabled:
+                    console.print(
+                        f"[bold purple]REASONING:[/bold purple] {thinking}",
+                        style="white on black",
+                    )
+
+                err_count = 0
             except Exception as e:
                 error_console.print(f"[red]ERROR: {str(e)}[/red]")
+                err_count += 0
+
+            if err_count == 2:
+                break
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
--- de_mcp_server.py
+++ de_mcp_server.py
@@ -12,7 +12,7 @@ session = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
-    region_name=os.getenv("AWS_DEFAULT_REGION")
+    region_name=os.getenv("AWS_DEFAULT_REGION"),
 )
 
 
@@ -23,29 +23,33 @@ def s3_upload_file(local_file_path: str, s3_bucket: str, s3_key: str):
         "status": "success",
         "content": [
             {"text": str(s3.upload_file(local_file_path, s3_bucket, s3_key))},
-        ]
+        ],
     }
 
 
 @mcp.tool
 def available_files_to_upload():
-    return  {
+    return {
         "status": "success",
         "content": [
-            {"json":  os.listdir('data')},
-        ]
+            {"json": os.listdir("data")},
+        ],
     }
 
 
 @mcp.tool
-def create_table(s3_bucket: str, s3_key: str, table_name: str, database_name:str) -> int:
+def create_table(
+    s3_bucket: str, s3_key: str, table_name: str, database_name: str
+) -> int:
     """Given a csv file s3 path, creates a Glue table"""
     file_s3_path = f"s3://{s3_bucket}/{s3_key}"
     table_location = f"s3://{s3_bucket}/processed/{table_name}"
 
     print(file_s3_path, table_name, database_name)
     df = wr.s3.read_csv(file_s3_path, boto3_session=session)
-    wr.catalog.delete_table_if_exists(database=database_name, table=table_name, boto3_session=session)
+    wr.catalog.delete_table_if_exists(
+        database=database_name, table=table_name, boto3_session=session
+    )
     temp_path = f"s3://{os.getenv("AWS_DATA_BUCKET")}/athena-temp/"
     wr.athena.to_iceberg(
         df=df,
@@ -58,9 +62,13 @@ def create_table(s3_bucket: str, s3_key: str, table_name: str, database_name:str
         "status": "success",
         "content": [
             {"text": f"Table '{table_name}' Created."},
-        ]
+        ],
     }
 
 
 if __name__ == "__main__":
-    mcp.run(transport="sse", host="127.0.0.1", port=8000)
+    mcp.run(
+        transport="sse",
+        host=os.getenv("MCP_SERVER_HOST"),
+        port=int(os.getenv("MCP_SERVER_PORT")),
+    )
